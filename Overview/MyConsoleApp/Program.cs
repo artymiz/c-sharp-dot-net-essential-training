32669028621955e96717dee6734a90a367f7464c
arty <arty@sfu.ca>
2023-05-29 20:09:44 -0700

    create a Hello console app

---
+++ Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What is your name?");
+
+string? response;
+response = Console.ReadLine();
+
+while (response == null || response == "")
+{
+    Console.WriteLine("Please enter your name.");
+    response = Console.ReadLine();
+}
+
+Console.WriteLine($"Hello, {response}!");
+
+OperatingSystem os = Environment.OSVersion;
+Console.WriteLine(os.Platform);
+Console.WriteLine(os.VersionString);
