5893208bcee2c91a50e346e2ae547099a0e5c79a
arty <arty@sfu.ca>
2023-06-02 18:47:58 -0700

    file information

--- Files/FileInfo/Program.cs
+++ Files/FileInfo/Program.cs
@@ -11,10 +11,24 @@ if (!File.Exists(filename)) {
 }
 
 // TODO: Get some information about the file
+// Console.WriteLine(File.GetCreationTime(filename));
+// Console.WriteLine(File.GetLastWriteTime(filename));
+// Console.WriteLine(File.GetLastAccessTime(filename));
+//
+// File.SetAttributes(filename, FileAttributes.ReadOnly);
+// Console.WriteLine(File.GetAttributes(filename));
 
-
-// TODO: We can also get general information using a FileInfo 
-
+// TODO: We can also get general information using a FileInfo
+try {
+    FileInfo fi = new FileInfo(filename);
+    Console.WriteLine(fi.Length);
+    Console.WriteLine(fi.Directory);
+    Console.WriteLine($"ReadOnly: {fi.IsReadOnly}");
+} catch (Exception e) {
+    Console.WriteLine($"Exception: {e.Message}");
+}
 
 // TODO: File information can also be manipulated
+File.SetCreationTime(filename, new DateTime(2020, 1, 1));
+Console.WriteLine($"Created on: {File.GetCreationTime(filename)}");
 
