7e655bd231a5fad467dbcf2a14182b51ac28b4ca
arty <arty@sfu.ca>
2023-06-02 18:28:14 -0700

    file creation

--- Files/CreateFiles/Program.cs
+++ Files/CreateFiles/Program.cs
@@ -5,6 +5,14 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
+using(StreamWriter writer = File.CreateText(filename)) {
+    writer.WriteLine("This is a test of creating a file in C#");
+}
 
 
 // TODO: Determine if a file exists
+Console.WriteLine(File.Exists(filename));
+if (File.Exists(filename)) {
+    File.Delete(filename);
+}
+
